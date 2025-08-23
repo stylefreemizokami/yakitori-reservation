caa60561ae9fc5c1b02897cb208152fada21c01c
stylefreemizokami <stylefree.mizokami@gmail.com>
2025-08-24 07:23:43 +0900

    変更内容

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,17 @@ import mysql from 'mysql2/promise'
 
 export async function getConnection() {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ay2DzVx8T27T',
-    database: 'mysql',
+    // local
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'Ay2DzVx8T27T',
+    // database: 'mysql',
+    
+    // dev
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   });
   return connection;
 }
