2b18483c9a6c97e742d99642d9f787b9779691db
stylefreemizokami <stylefree.mizokami@gmail.com>
2025-08-24 07:37:48 +0900

    変更内容

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,12 +7,12 @@ export async function getConnection() {
     // user: 'root',
     // password: 'Ay2DzVx8T27T',
     // database: 'mysql',
-    
+
     // dev
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   });
   return connection;
 }
