8ee936ca63f730c4a9026e8a3c3564ab43d2b984
stylefreemizokami <stylefree.mizokami@gmail.com>
2025-08-24 20:17:02 +0900

    変更内容

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // ビルド時に ESLint を無視
+  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
